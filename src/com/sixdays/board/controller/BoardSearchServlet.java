a3b84a7e55b665f48932547d06a436283b27c2cc
Shin kyungseob <abcdfabcdf@naver.com>
2020-02-17 21:02:15 +0900

    공지사항 검색, 페이징

--- src/com/sixdays/board/controller/BoardSearchServlet.java
+++ src/com/sixdays/board/controller/BoardSearchServlet.java
@@ -1,11 +1,17 @@
 package com.sixdays.board.controller;
 
 import java.io.IOException;
+import java.util.ArrayList;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.sixdays.board.model.service.BoardService;
+import com.sixdays.board.model.vo.Board;
+import com.sixdays.board.model.vo.PageInfo;
 
 /**
  * Servlet implementation class BoardSearchServlet
@@ -26,8 +32,65 @@ public class BoardSearchServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		String category = request.getParameter("con");
+		
+		String keyword = request.getParameter("keyword");
+		
+		System.out.println("category : " + category);
+		System.out.println("keyword : " + keyword);
+		ArrayList<Board> list = new ArrayList<>();
+		
+		BoardService bs = new BoardService();
+		
+		
+		int startPage;
+		int endPage;
+		int maxPage;
+		int currentPage;
+		int limit;
+		
+		currentPage = 1;
+		
+		limit = 10;
+		
+		if(request.getParameter("currentPage") != null) {
+			currentPage = Integer.parseInt(request.getParameter("currentPage"));
+		}
+		
+		int listCount = bs.getListSubCount(keyword);
+		
+		System.out.println("listcount :" + listCount);
+		
+		maxPage = (int)((double)listCount / limit + 0.9);
+		
+		
+		startPage = ((int)((double)currentPage / limit + 0.9)-1) * limit + 1;
+		
+		endPage = startPage + limit - 1;
+		
+		if(endPage > maxPage) {
+			endPage = maxPage;
+		}
+		
+		list = bs.searchBoard(category, keyword, currentPage, limit);
+		
+		String page = "";
+		
+		if(list != null) {
+			page = "views/board/BoardList.jsp";
+			request.setAttribute("list", list);
+			
+			PageInfo pi = new PageInfo(currentPage,listCount,limit,maxPage,startPage,endPage);
+			request.setAttribute("pi", pi);
+			
+			request.setAttribute("category", category);
+			request.setAttribute("keyword", keyword);
+			
+		} else {
+			page = "";
+			request.setAttribute("msg", "검색 실패");
+		}
+		request.getRequestDispatcher(page).forward(request, response);
 	}
 
 	/**
